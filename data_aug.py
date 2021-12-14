9ee4495cac147bb01cebe05bb537b3c110ac4931
TalhaOmi <talha.talukder@northsouth.edu>
2021-12-14 12:33:40 +0600

    after creating data folder

--- data_aug.py
+++ data_aug.py
@@ -1,12 +1,10 @@
 import os
 import numpy as np
 import cv2
-import torch
 from glob import glob
 from tqdm import tqdm
 import imageio
 from albumentations import HorizontalFlip, VerticalFlip, Rotate
-
 
 """ Create a directory """
 def create_dir(path):
@@ -22,7 +20,6 @@ def load_data(path):
 
     return (train_x, train_y), (test_x, test_y)
 
-
 def augment_data(images, masks, save_path, augment=True):
     size = (512, 512)
 
@@ -35,11 +32,29 @@ def augment_data(images, masks, save_path, augment=True):
         y = imageio.mimread(y)[0]
 
         if augment == True:
-            pass
+            aug = HorizontalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x1 = augmented["image"]
+            y1 = augmented["mask"]
+
+
+            aug = VerticalFlip(p=1.0)
+            augmented = aug(image=x, mask=y)
+            x2 = augmented["image"]
+            y2 = augmented["mask"]
+
+            aug = Rotate(limit=45, p=1.0)
+            augmented = aug(image=x, mask=y)
+            x3 = augmented["image"]
+            y3 = augmented["mask"]
+
+            X = [x, x1, x2, x3]
+            Y = [y, y1, y2, y3]
 
         else:
             X = [x]
             Y = [y]
+
 
         index = 0
         for i, m in zip(X, Y):
@@ -48,7 +63,7 @@ def augment_data(images, masks, save_path, augment=True):
 
             tmp_image_name = f"{name}_{index}.png"
             tmp_mask_name = f"{name}_{index}.png"
-
+            print(tmp_image_name)
             image_path = os.path.join(save_path, "image", tmp_image_name)
             mask_path = os.path.join(save_path, "mask", tmp_mask_name)
 
@@ -57,26 +72,27 @@ def augment_data(images, masks, save_path, augment=True):
 
             index += 1
 
-        break
+            break
+
 
 
 if __name__ == "__main__":
     """ Seeding """
     np.random.seed(42)
 
-    """ Load the dataset """
+    """ Load the data """
     data_path = "dataset/"
     (train_x, train_y), (test_x, test_y) = load_data(data_path)
 
     print(f"Train: {len(train_x)} - {len(train_y)}")
     print(f"Test: {len(test_x)} - {len(test_y)}")
 
-
-    """ Create directories  to save the augmented data """
+    """ Create directories to save the augmented data """
     create_dir("new_data/train/image/")
     create_dir("new_data/train/mask/")
     create_dir("new_data/test/image/")
     create_dir("new_data/test/mask/")
 
+
     """ Data augmentation """
-    augment_data(train_x, train_y, "/new_data/train/", augment=False)
+    augment_data(train_x, train_y, "new_data/train/", augment=True)
